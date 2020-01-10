c48ef1508fad6a48395e9209cb5ef0ce87c7e703
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-01-10 15:33:55 +0000

    changed ID names

--- recent_search/models.py
+++ recent_search/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class RecentSearch(models.Model):
-    spotifyId = models.CharField(max_length=64)
+    deezerId = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
     
     def __str__(self):
-        return f'Recent Search {self.spotifyId} - {self.name}'
+        return f'Recent Search {self.deezerId} - {self.name}'
