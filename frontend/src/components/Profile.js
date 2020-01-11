7500261d754ca6c2bf8afbf8a40c5d3e8946e9b5
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-01-11 11:53:45 +0000

    removed package lock

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import moment from 'moment'
 import Auth from '../lib/authMethods'
+import distance from '../lib/distanceMethod'
 
 const initialArtists = [{ artists: {} }]
 const initialSingleArtist = { id: '', name: '', picture_medium: '' }
@@ -45,6 +47,7 @@ const Profile = () => {
         const skiddleId = resp.data.results[0].id
         axios.get(`https://cors-anywhere.herokuapp.com/www.skiddle.com/api/v1/events/search/?api_key=${API_KEY}&a=${skiddleId}&country=GB`)
           .then(resp => {
+            console.log(resp.data)
             setGigs(resp.data.results)
           })
       })
@@ -56,16 +59,18 @@ const Profile = () => {
     getSkiddleGigs(artist)
   }
 
-  return <section className="section">
+  console.log(`${(distance(51.437291, -0.255294, 51.413792, -0.180206) * 0.000621371).toFixed(1)} miles`)
+  
+  return <section className="section" id="profile">
     <div className="container">
       <div className="columns">
-        <div className="column">
+        <div className="column" id="artist">
           <div className="title has-text-centered">Artists</div>
           {artists.map((artist, i) => {
             return <div key={i} title={artist.name} onClick={handleClick}>{artist.name}</div>
           })}
         </div>
-        <div className="column">
+        <div className="column" id="single-artist">
           <div className="title has-text-centered">Selected Artist</div>
           <div className="subtitle">{singleArtist.name}</div>
           <img src={singleArtist.picture_medium} alt=""/>
@@ -77,11 +82,11 @@ const Profile = () => {
             </div>
           })}
         </div>
-        <div className="column">
+        <div className="column" id="gigs">
           <div className="title has-text-centered">Gigs</div>
           {gigs.map((gig, i) => {
             return <div key={i}>
-              <p>{gig.venue.name} - {gig.venue.town}</p>
+              <p>{gig.venue.town} - {gig.venue.name} - {moment(gig.startdate).format('MMMM Do YYYY, h:mm a')}</p>
             </div>
           })}
         </div>
