4d14c32eea10b6c06049ba522ee830b42d267a1b
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-01-14 10:01:33 +0000

    added locate me button

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import ReactAudioPlayer from 'react-audio-player'
 import axios from 'axios'
 import moment from 'moment'
 import Auth from '../lib/authMethods'
@@ -18,7 +19,7 @@ const Profile = () => {
   const [gigs, setGigs] = useState(initialGigs)
   const [cities, setCities] = useState([])
   const [userPosition, setUserPosition] = useState({ latitude: '', longitude: '' })
-
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     axios.get('/api/profile', {
@@ -83,6 +84,7 @@ const Profile = () => {
 
   function getLocation() {
     if (navigator.geolocation) {
+      setLoading(true)
       navigator.geolocation.getCurrentPosition(setPosition)
     } else {
       console.log('geolocation not supported')
@@ -91,11 +93,11 @@ const Profile = () => {
 
   function setPosition(position) {
     setUserPosition({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+    setLoading(false)
   }
   
   return <section className="section" id="profile">
     <div className="container">
-      <div className="button is-link" onClick={getLocation}>Get my location</div>
       <div className="columns">
         <div className="column" id="artists">
           <div className="title has-text-centered has-text-white">Artists</div>
@@ -108,27 +110,24 @@ const Profile = () => {
           <div className="subtitle">{singleArtist.name}</div>
           <img src={singleArtist.picture_medium} alt=""/>
           {songPreviews.length === 0 ? null : <div>Top tracks</div>}
-          {songPreviews.map((song, i) => {
+          {songPreviews.slice(0,3).map((song, i) => {
             return <div key={i}>
               <p>{song.title}</p>
-              <audio src={song.preview}></audio>
+              <ReactAudioPlayer src={song.preview} controls/>
             </div>
           })}
         </div>
         <div className="column" id="gigs">
-          <div className="title has-text-centered">Gigs</div>
-          {/* {gigs.map((gig, i) => {
-            return <div key={i}>
-              <p>{gig.venue.town} - {gig.venue.name} - {moment(gig.startdate).format('MMMM Do YYYY, h:mm a')}</p>
-              <p>{gig.venue.town} - {gig.venue.name}{userPosition.latitude ? ` - ${distance(userPosition.latitude, userPosition.longitude, gig.venue.latitude, gig.venue.longitude)} miles` : null}</p>
-            </div>
-          })} */}
+          <div id="test">
+            <div className={!loading ? 'button is-small' : 'button is-small is-loading'} onClick={getLocation}>Locate me</div>
+            <p className="title">Gigs</p>
+          </div>
           {gigs.map((city, i) => {
             return <div key={i}>
               <p className="title">{city[cities[i]][0].venue.town}</p>
               {city[cities[i]].map((gig, j) => {
                 return <div key={j}>
-                  <p>{moment(gig.startdate).format('MMMM Do YYYY, h:mma')}</p>
+                  <p>{gig.venue.name} - {moment(gig.startdate).format('MMM Do YYYY')} {userPosition.latitude ? ` - ${distance(userPosition.latitude, userPosition.longitude, gig.venue.latitude, gig.venue.longitude)} miles away` : null}</p>
                 </div>
               })}
             </div>
