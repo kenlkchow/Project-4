91e20c0fa164443eba86408daae45d94496e7700
dredizzle <dredizzle@live.co.uk>
2020-01-15 10:59:00 +0000

    various styling and adding suggestions functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = () => {
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
         <div className="title" id="logo-title">
-          <div className="bracket-nav is-size-8 logo">[</div>
+          {/* <div className="bracket-nav is-size-8 logo">[</div> */}
           <div className="title-name is-size-6 has-text-weight-bold logo">artist_Flow</div>
-          <div className="bracket-nav is-size-8 logo">]</div>
+          {/* <div className="bracket-nav is-size-8 logo">]</div> */}
         </div>
       </Link>
     </div>
-    <div className="navbar-end">
+    <div className="navbar-start">
       <div className="navbar-item">
         <Link to="/profile">Profile</Link>
       </div>
